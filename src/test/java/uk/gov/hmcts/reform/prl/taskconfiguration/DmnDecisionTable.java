3c18322577e807f7acf5e4428416dc9998d207c3
Sathish Kannan <sathish.kannan@hmcts.net>
2022-09-16 21:13:35 +0100

    https://tools.hmcts.net/jira/browse/ISDB-383 - Created the DMN files for Private Law and its configuration

--- src/test/java/uk/gov/hmcts/reform/prl/taskconfiguration/DmnDecisionTable.java
+++ src/test/java/uk/gov/hmcts/reform/prl/taskconfiguration/DmnDecisionTable.java
@@ -3,12 +3,12 @@ package uk.gov.hmcts.reform.prl.taskconfiguration;
 import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum DmnDecisionTable {
-    WA_TASK_ALLOWED_DAYS_PRL("wa-task-allowed-days-prl", "wa-task-allowed-days-prl.dmn"),
-    WA_TASK_CANCELLATION_PRL("wa-task-cancellation-prl", "wa-task-cancellation-prl.dmn"),
-    WA_TASK_COMPLETION_PRL("wa-task-completion-prl", "wa-task-completion-prl.dmn"),
-    WA_TASK_CONFIGURATION_PRL("wa-task-configuration-prl", "wa-task-configuration-prl.dmn"),
-    WA_TASK_INITIATION_PRL("wa-task-initiation-prl", "wa-task-initiation-prl.dmn"),
-    WA_TASK_PERMISSIONS_PRL("wa-task-permissions-prl", "wa-task-permissions-prl.dmn");
+    WA_TASK_ALLOWED_DAYS_PRL("wa-task-allowed-days-prl", "wa-task-allowed-days-prlapps-prl.dmn"),
+    WA_TASK_CANCELLATION_PRL("wa-task-cancellation-prl", "wa-task-cancellation-prlapps-prl.dmn"),
+    WA_TASK_COMPLETION_PRL("wa-task-completion-prl", "wa-task-completion-prlapps-prl.dmn"),
+    WA_TASK_CONFIGURATION_PRL("wa-task-configuration-prl", "wa-task-configuration-prlapps-prl.dmn"),
+    WA_TASK_INITIATION_PRL("wa-task-initiation-prl", "wa-task-initiation-prlapps-prl.dmn"),
+    WA_TASK_PERMISSIONS_PRL("wa-task-permissions-prl", "wa-task-permissions-prlapps-prl.dmn");
 
     @JsonValue
     private final String key;
