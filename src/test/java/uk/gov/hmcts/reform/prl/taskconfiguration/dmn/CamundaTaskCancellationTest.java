29d05ca36448182231e9e6f98a8b29e8545fdc8c
Sathish Kannan <sathish.kannan@hmcts.net>
2024-02-13 17:06:34 +0000

    relaxing statementOfService for performing this event without task

--- src/test/java/uk/gov/hmcts/reform/prl/taskconfiguration/dmn/CamundaTaskCancellationTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/taskconfiguration/dmn/CamundaTaskCancellationTest.java
@@ -59,6 +59,10 @@ class CamundaTaskCancellationTest extends DmnDecisionTableBaseUnitTest {
                     Map.of(
                         "action", "Cancel",
                         "processCategories", "statementOfServiceBySolicitor"
+                    ),
+                    Map.of(
+                        "action", "Cancel",
+                        "processCategories", "arrangeBailiffSOA"
                     )
                 )
             ),
@@ -157,7 +161,7 @@ class CamundaTaskCancellationTest extends DmnDecisionTableBaseUnitTest {
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
         assertThat(logic.getInputs().size(), is(3));
         assertThat(logic.getOutputs().size(), is(4));
-        assertThat(logic.getRules().size(), is(12));
+        assertThat(logic.getRules().size(), is(13));
 
     }
 }
