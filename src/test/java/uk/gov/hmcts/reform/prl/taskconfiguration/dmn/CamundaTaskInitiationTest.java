ac3528f824bd2c7288a7a873e7945516f9a4aa80
alokDatta <aloknath.datta@hmcts.net>
2024-05-01 11:48:05 +0100

    Admin create added

--- src/test/java/uk/gov/hmcts/reform/prl/taskconfiguration/dmn/CamundaTaskInitiationTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/taskconfiguration/dmn/CamundaTaskInitiationTest.java
@@ -37,7 +37,7 @@ class CamundaTaskInitiationTest extends DmnDecisionTableBaseUnitTest {
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
         assertThat(logic.getInputs().size(), is(20));
         assertThat(logic.getOutputs().size(), is(4));
-        assertThat(logic.getRules().size(), is(96));
+        assertThat(logic.getRules().size(), is(97));
     }
 
     static Stream<Arguments> scenarioProvider() {
