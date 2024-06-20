ae062525c5961053a38f7caac47df7430df91396
mohitvijay45 <107183474+mohitvijay45@users.noreply.github.com>
2024-06-20 15:25:30 +0100

    Update CamundaTaskCancellationTest.java

--- src/test/java/uk/gov/hmcts/reform/prl/taskconfiguration/dmn/CamundaTaskCancellationTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/taskconfiguration/dmn/CamundaTaskCancellationTest.java
@@ -145,6 +145,10 @@ class CamundaTaskCancellationTest extends DmnDecisionTableBaseUnitTest {
                                 Map.of(
                                         "action", "Cancel",
                                         "processCategories", "createHearingRequest"
+                                ),
+                                Map.of(
+                                        "action", "Cancel",
+                                        "processCategories", "newCaseTransferredToCourt"
                                 )
                         )
                 )
@@ -173,7 +177,7 @@ class CamundaTaskCancellationTest extends DmnDecisionTableBaseUnitTest {
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
         assertThat(logic.getInputs().size(), is(3));
         assertThat(logic.getOutputs().size(), is(4));
-        assertThat(logic.getRules().size(), is(15));
+        assertThat(logic.getRules().size(), is(14));
 
     }
 }
