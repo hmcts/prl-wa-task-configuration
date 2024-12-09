6eeb155ad73a145575202f6ddd1724be1cb5591b
sairamkantheti1 <sai.kantheti@HMCTS.NET>
2024-12-09 13:18:40 +0000

    INC5668965 confidential check unlinked from tasks

--- src/test/java/uk/gov/hmcts/reform/prl/taskconfiguration/dmn/CamundaTaskCompletionTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/taskconfiguration/dmn/CamundaTaskCompletionTest.java
@@ -318,7 +318,8 @@ class CamundaTaskCompletionTest extends DmnDecisionTableBaseUnitTest {
                     Map.of(
                         "taskType", "confidentialCheckSOA",
                         "completionMode", "Auto"
-                    )
+                    ),
+                    Map.of()
                 )
             ),
             Arguments.of(
