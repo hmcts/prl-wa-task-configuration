b031c1a263b504610480eec38a00dae511eedb39
SomeshRA <somesh.acharya1@hmcts.net>
2025-05-14 18:23:37 +0100

    FPVTL-499: Changes moved to cancellation DMN

--- src/test/java/uk/gov/hmcts/reform/prl/taskconfiguration/dmn/CamundaTaskCompletionTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/taskconfiguration/dmn/CamundaTaskCompletionTest.java
@@ -388,16 +388,6 @@ class CamundaTaskCompletionTest extends DmnDecisionTableBaseUnitTest {
                     ),
                     Map.of()
                 )
-            ),
-            Arguments.of(
-                "citizenStatementOfService",
-                List.of(
-                    Map.of(
-                        "taskType", "appStatementOfServiceByLiP",
-                        "completionMode", "Auto"
-                    ),
-                    Map.of()
-                )
             )
         );
     }
@@ -420,7 +410,7 @@ class CamundaTaskCompletionTest extends DmnDecisionTableBaseUnitTest {
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
         assertThat(logic.getInputs().size(), is(1));
         assertThat(logic.getOutputs().size(), is(2));
-        assertThat(logic.getRules().size(), is(68));
+        assertThat(logic.getRules().size(), is(67));
     }
 
 
