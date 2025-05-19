87c0c6887a47a1142a5d9c5ef550ef44fb066a02
sunilrochani <sunil.rochani@hotmail.com>
2025-05-19 17:26:02 +0100

    FPVTL-494: 'Review Additional Application' task update

--- src/test/java/uk/gov/hmcts/reform/prl/taskconfiguration/dmn/CamundaTaskCancellationTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/taskconfiguration/dmn/CamundaTaskCancellationTest.java
@@ -189,7 +189,7 @@ class CamundaTaskCancellationTest extends DmnDecisionTableBaseUnitTest {
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
         assertThat(logic.getInputs().size(), is(3));
         assertThat(logic.getOutputs().size(), is(4));
-        assertThat(logic.getRules().size(), is(18));
+        assertThat(logic.getRules().size(), is(17));
 
     }
 }
