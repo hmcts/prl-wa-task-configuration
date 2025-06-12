f81070a45bf3ec95a302e59b28be94f43838c960
SomeshRA <somesh.acharya1@hmcts.net>
2025-06-12 17:26:19 +0100

    FPVTL-498: Merging with master

--- src/test/java/uk/gov/hmcts/reform/prl/taskconfiguration/dmn/CamundaTaskConfigurationTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/taskconfiguration/dmn/CamundaTaskConfigurationTest.java
@@ -33,9 +33,9 @@ class CamundaTaskConfigurationTest extends DmnDecisionTableBaseUnitTest {
     void if_this_test_fails_needs_updating_with_your_changes() {
         //The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
-        assertThat(logic.getInputs().size(), is(4));
+        assertThat(logic.getInputs().size(), is(5));
         assertThat(logic.getOutputs().size(), is(3));
-        assertThat(logic.getRules().size(), is(98));
+        assertThat(logic.getRules().size(), is(99));
     }
 
 
