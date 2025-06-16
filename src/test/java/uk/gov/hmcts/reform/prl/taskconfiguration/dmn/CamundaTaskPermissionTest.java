e5ea24fe8bb2d55b4bba250b7b78ec06b27f3984
SomeshRA <somesh.acharya1@hmcts.net>
2025-06-16 11:05:44 +0100

    FPVLT-493: Added hearing admin permission

--- src/test/java/uk/gov/hmcts/reform/prl/taskconfiguration/dmn/CamundaTaskPermissionTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/taskconfiguration/dmn/CamundaTaskPermissionTest.java
@@ -595,15 +595,10 @@ class CamundaTaskPermissionTest extends DmnDecisionTableBaseUnitTest {
         )));
     }
 
-    @SuppressWarnings("checkstyle:indentation")
-    @ParameterizedTest
-    @CsvSource(value = {
-        "removeLegalRepresentativeC100",
-        "reviewLangAndSmReq"
-    })
-    void evaluate_task_admin_removeLegalRepresentativeC100(String taskType) {
+    @Test
+    void evaluate_task_admin_removeLegalRepresentativeC100() {
         VariableMap inputVariables = new VariableMapImpl();
-        inputVariables.putValue("taskAttributes", Map.of("taskType", taskType));
+        inputVariables.putValue("taskAttributes", Map.of("taskType", "removeLegalRepresentativeC100"));
 
         DmnDecisionTableResult dmnDecisionTableResult = evaluateDmnTable(inputVariables);
 
@@ -620,6 +615,34 @@ class CamundaTaskPermissionTest extends DmnDecisionTableBaseUnitTest {
                 "value", "Read,Own,UnclaimAssign,Claim,Unclaim,UnassignClaim"
             )
         )));
+    }
+
+    @Test
+    void evaluate_task_admin_reviewLangAndSmReq() {
+        VariableMap inputVariables = new VariableMapImpl();
+        inputVariables.putValue("taskAttributes", Map.of("taskType", "reviewLangAndSmReq"));
+
+        DmnDecisionTableResult dmnDecisionTableResult = evaluateDmnTable(inputVariables);
+
+        MatcherAssert.assertThat(dmnDecisionTableResult.getResultList(), is(List.of(
+            Map.of(
+                "autoAssignable", false,
+                "name", "task-supervisor",
+                "value", "Read,Manage,Complete,Cancel,Assign,Unassign"
+            ),Map.of(
+                "autoAssignable", false,
+                "name", "hearing-centre-admin",
+                "roleCategory", "ADMIN",
+                "authorisations", "SKILL:ABA5:CHECKAPPLICATIONC100",
+                "value", "Read,Own,UnclaimAssign,Claim,Unclaim,UnassignClaim"
+            ),Map.of(
+                "autoAssignable", false,
+                "name", "hearing-centre-team-leader",
+                "roleCategory", "ADMIN",
+                "authorisations", "SKILL:ABA5:CHECKAPPLICATIONC100",
+                "value", "Read,Own,UnclaimAssign,Claim,Unclaim,UnassignClaim,Complete"
+            )
+            )));
     }
 
     @SuppressWarnings("checkstyle:indentation")
@@ -1121,7 +1144,7 @@ class CamundaTaskPermissionTest extends DmnDecisionTableBaseUnitTest {
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
         assertThat(logic.getInputs().size(), is(2));
         assertThat(logic.getOutputs().size(), is(7));
-        assertThat(logic.getRules().size(), is(41));
+        assertThat(logic.getRules().size(), is(42));
     }
 
     @ParameterizedTest
