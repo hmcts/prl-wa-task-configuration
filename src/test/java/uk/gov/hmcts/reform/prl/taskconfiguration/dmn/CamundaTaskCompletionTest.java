a0448b250937132f96c07a6d4a6b67e352a63663
sunilrochani <sunil.rochani@hotmail.com>
2025-06-16 10:23:27 +0100

    FPVTL-497: 'Review RA request' task update

--- src/test/java/uk/gov/hmcts/reform/prl/taskconfiguration/dmn/CamundaTaskCompletionTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/taskconfiguration/dmn/CamundaTaskCompletionTest.java
@@ -432,7 +432,7 @@ class CamundaTaskCompletionTest extends DmnDecisionTableBaseUnitTest {
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
         assertThat(logic.getInputs().size(), is(1));
         assertThat(logic.getOutputs().size(), is(2));
-        assertThat(logic.getRules().size(), is(70));
+        assertThat(logic.getRules().size(), is(71));
     }
 
 
