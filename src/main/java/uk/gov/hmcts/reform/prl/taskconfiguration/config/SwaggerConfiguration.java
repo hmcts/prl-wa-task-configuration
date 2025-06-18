388387cce0a3979c81350178d004f7c5d237c59d
Michael1142 <michael1142@hotmail.co.uk>
2025-06-18 16:11:10 +0100

    Spring boot 3 upgrade

--- src/main/java/uk/gov/hmcts/reform/prl/taskconfiguration/config/SwaggerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/prl/taskconfiguration/config/SwaggerConfiguration.java
@@ -1,25 +1,18 @@
 package uk.gov.hmcts.reform.prl.taskconfiguration.config;
 
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import uk.gov.hmcts.reform.prl.taskconfiguration.Application;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfiguration {
 
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-            .useDefaultResponseMessages(false)
-            .select()
-            .apis(RequestHandlerSelectors.basePackage(Application.class.getPackage().getName() + ".controllers"))
-            .paths(PathSelectors.any())
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+            .group("default")
+            .packagesToScan(Application.class.getPackageName() + ".controllers")
             .build();
     }
 
