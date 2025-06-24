fcd2a3323263c6092ba5e187d18d63fe6ec9439f
sanjayparekh-hmcts <sanjay.parekh@hmcts.net>
2025-06-24 15:51:57 +0100

    FPVTL-724: corrected test after merge

--- src/test/java/uk/gov/hmcts/reform/prl/taskconfiguration/dmn/CamundaTaskPermissionTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/taskconfiguration/dmn/CamundaTaskPermissionTest.java
@@ -1154,7 +1154,7 @@ class CamundaTaskPermissionTest extends DmnDecisionTableBaseUnitTest {
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
         assertThat(logic.getInputs().size(), is(2));
         assertThat(logic.getOutputs().size(), is(7));
-        assertThat(logic.getRules().size(), is(41));
+        assertThat(logic.getRules().size(), is(42));
     }
 
     @ParameterizedTest
