b0d5023b955239e7a2a11f68b345aab55e1b73b8
SomeshRA <somesh.acharya1@hmcts.net>
2025-12-03 15:07:06 +0000

    sendToGateKeeperC100 task added assignee

--- src/test/java/uk/gov/hmcts/reform/prl/taskconfiguration/dmn/CamundaTaskConfigurationTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/taskconfiguration/dmn/CamundaTaskConfigurationTest.java
@@ -35,7 +35,7 @@ class CamundaTaskConfigurationTest extends DmnDecisionTableBaseUnitTest {
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
         assertThat(logic.getInputs().size(), is(6));
         assertThat(logic.getOutputs().size(), is(3));
-        assertThat(logic.getRules().size(), is(102));
+        assertThat(logic.getRules().size(), is(103));
     }
 
 
