559357003be293fe00e78164ba0aa1d38b68084e
Jude-hmcts <Jude.Lawrence@HMCTS.NET>
2026-04-01 13:32:53 +0100

    test fixed to match the no of rows

--- src/test/java/uk/gov/hmcts/reform/prl/taskconfiguration/dmn/CamundaTaskInitiationTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/taskconfiguration/dmn/CamundaTaskInitiationTest.java
@@ -41,7 +41,7 @@ class CamundaTaskInitiationTest extends DmnDecisionTableBaseUnitTest {
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
         assertThat(logic.getInputs().size(), is(25));
         assertThat(logic.getOutputs().size(), is(4));
-        assertThat(logic.getRules().size(), is(121));
+        assertThat(logic.getRules().size(), is(123));
     }
 
     static Stream<Arguments> scenarioProvider() {
