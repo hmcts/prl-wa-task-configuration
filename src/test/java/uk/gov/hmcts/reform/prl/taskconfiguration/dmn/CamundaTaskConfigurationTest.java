6948e6306838d949280d6483b8a0df5d79bce262
SomeshRA <somesh.acharya1@hmcts.net>
2026-05-21 17:06:38 +0100

    FPVTL-2895: Align Request CIR Update task flow with Send & Reply journey

--- src/test/java/uk/gov/hmcts/reform/prl/taskconfiguration/dmn/CamundaTaskConfigurationTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/taskconfiguration/dmn/CamundaTaskConfigurationTest.java
@@ -21,7 +21,9 @@ import java.util.Map;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
 //TODO add test for requestCirUpdate
+
 class CamundaTaskConfigurationTest extends DmnDecisionTableBaseUnitTest {
 
     @BeforeAll
