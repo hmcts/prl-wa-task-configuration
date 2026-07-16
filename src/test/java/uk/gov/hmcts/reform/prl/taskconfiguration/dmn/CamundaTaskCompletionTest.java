0b9a727b69803a7a948a21765d3b85c96eeca20d
sanjayparekh-hmcts <sanjay.parekh@hmcts.net>
2026-07-16 12:38:05 +0100

    FPVTL-2409: merged from master

--- src/test/java/uk/gov/hmcts/reform/prl/taskconfiguration/dmn/CamundaTaskCompletionTest.java
+++ src/test/java/uk/gov/hmcts/reform/prl/taskconfiguration/dmn/CamundaTaskCompletionTest.java
@@ -520,7 +520,7 @@ class CamundaTaskCompletionTest extends DmnDecisionTableBaseUnitTest {
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
         assertThat(logic.getInputs().size(), is(2));
         assertThat(logic.getOutputs().size(), is(2));
-        assertThat(logic.getRules().size(), is(78));
+        assertThat(logic.getRules().size(), is(79));
     }
 
 
